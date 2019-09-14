ec801afdb105c708a9ccd35f79fe4ed847f02ae5
Daniel Estevez <daniel.ef@gmail.com>
2019-09-13 21:57:19 -0500

    Add tray mode

--- src/hello_fx/hello.clj
+++ src/hello_fx/hello.clj
@@ -2,8 +2,72 @@
   (:gen-class :extends
               javafx.application.Application))
 
+(defn button [text]
+  (doto (javafx.scene.control.Button.)
+    (.setText (str "Say " text))
+    (.setOnAction (proxy [javafx.event.EventHandler] []
+                    (handle [event]
+                      (println text))))))
+
+(defn hide [stage]
+  (javafx.application.Platform/runLater 
+   (fn []
+     (try 
+       (println (pr-str {:system-tray-support (java.awt.SystemTray/isSupported)}))
+       (if (java.awt.SystemTray/isSupported)
+         (.hide stage))
+       (catch Throwable e
+         (.printStackTrace e))))))
+
+(defn create-tray-icon [stage]
+  (if (java.awt.SystemTray/isSupported)
+    (let [tray (java.awt.SystemTray/getSystemTray)
+          icon (try 
+                 (-> (ClassLoader/getSystemResource "foo.png")
+                     (javax.imageio.ImageIO/read))
+                 (catch Exception e
+                   (.printStackTrace e)))
+          popup (java.awt.PopupMenu.)
+          on-show-l  (reify java.awt.event.ActionListener
+                       (actionPerformed [_ action-event]
+                         (javafx.application.Platform/runLater
+                          #(.show stage))))
+          tray-icon (doto (java.awt.TrayIcon. icon "Foo!" popup)
+                      (.addActionListener on-show-l))
+          
+          on-close-r (reify javafx.event.EventHandler
+                       (handle [_ window-event]
+                         (hide stage)))
+
+          on-close-l (reify java.awt.event.ActionListener
+                       (actionPerformed [_ action-event]
+                         (System/exit 0)))
+          on-show (doto (java.awt.MenuItem. "show")
+                    (.addActionListener on-show-l))
+          on-close (doto (java.awt.MenuItem. "close")
+                     (.addActionListener on-close-l))
+          ]
+      (.setOnCloseRequest stage on-close-r)
+      (.add popup (java.awt.MenuItem. "Foo"))
+      (.addSeparator popup)
+      (.add popup on-show)
+      (.add popup on-close)
+      (-> tray
+          (.add tray-icon)))))
+
 (defn -start [this primary-stage]
-  (.show primary-stage))
+  (let [root (doto (javafx.scene.layout.StackPane.)
+               (-> (.getChildren)
+                   (.add (button "Hello World"))))]
+    (println (pr-str {:primary-stage primary-stage}))
+
+    (create-tray-icon primary-stage)
+    (javafx.application.Platform/setImplicitExit false)
+    (.setScene primary-stage (javafx.scene.Scene. root 300 300))
+    #_(.initStyle primary-stage javafx.stage.StageStyle/TRANSPARENT)
+    (.setAlwaysOnTop primary-stage true)
+    (.show primary-stage)
+    (hide primary-stage)))
 
 (defn -main [& args]
   (javafx.application.Application/launch hello_fx.hello 
